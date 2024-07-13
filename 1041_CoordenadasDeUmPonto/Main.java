195277fbf74bfa4f6e64df666847bec0e7e769e7
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-07-13 20:26:54 -0300

    Exercicio coordenadas resolvido

---
+++ 1041_CoordenadasDeUmPonto/Main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        float x, y;
+        
+        x = sc.nextFloat();
+        y = sc.nextFloat();
+        
+        if(x == 0 && y == 0){
+            System.out.println("Origem");
+        } 
+        else if (x > 0 && y > 0){
+            System.out.println("Q1");
+        }
+        else if (x < 0 && y > 0){
+            System.out.println("Q2");
+        }
+        else if (x < 0 && y < 0){
+            System.out.println("Q3");
+        }
+        else if (x > 0 && y < 0){
+            System.out.println("Q4");
+        }
+        else {
+            if(x == 0){System.out.println("Eixo Y");}
+            else if (y == 0){System.out.println("Eixo X");}
+        }
+        
+        sc.close();
+    }
+ 
+}
