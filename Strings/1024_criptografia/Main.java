91b9acc3cf9981f412a5f54be2f57c1fe421a67e
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-07-30 18:51:57 -0300

    Exercicio 1024 resolvido

--- Strings/1024_criptografia/Main.java
+++ Strings/1024_criptografia/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
  
@@ -11,66 +10,81 @@ public class Main {
         //Declaracao e leitura da variavel
         int N = Integer.parseInt(sc.readLine());
         //Declaracao do ArrayList
-        ArrayList<String> string = new ArrayList<String>();
+        String string[] = new String[N];
 
         /*Leitura do array list colocando um
          caractere de linha no final */
         for(int i = 0; i < N; i++){
-            string.add(sc.readLine() + "\n");
+            string[i] = (sc.readLine());
         }
-        
-        //Declarando array de String
-        String[] linhas = new String[N];
-        
-        /* Convertendo arrayList para array de String
-         * para facilitar a conversao da criptografia
+
+        /* Iteracao para enviar cada indice da String
+         * ao metodo da primeira criptografia
          */
-        for(int i = 0; i < N; i++){
-            linhas[i] = string.get(i);
+        for(int i = 0; i < string.length; i++){
+            string[i] = encriptografaPrimeiraVez(string[i]);
         }
 
-        /* Declaracao da variavel count e retorno da funcao que vai contar
-         * o espaco total do array de String e o espaco ocupado por cada
-         * caracter dentro das Strings
+        /* Iteracao para enviar cada indice do novo array de String
+         * ao metodo de reversao
          */
-        int count = espacoTotalString(linhas);
-
-        //Declaracao de array de int
-        int[] arrayDeCaracter = new int[count];
-
-        /* array recebe um array de string convertido em caracteres 
-         * respectivamente em forma de numeros da tabela ASCII
-        */
-        arrayDeCaracter = convertStringToInt(linhas, count);
-
-        for(int i = 0; i < arrayDeCaracter.length; i++){
-            System.out.println((char)arrayDeCaracter[i]);
+        for(int i = 0; i < string.length; i++){
+            string[i] = reverteString(string[i]);
         }
+
+        /* Iteracao para encriptografar a String pela
+         * segunda vez
+         */
+        for(int i = 0; i < string.length; i++){
+            string[i] = encriptografaSegundaVez(string[i]);
+        }
+
+        imprimir(string);
         
         sc.close();
-    } 
-
-    /* Metodo para retornar um array de String convertido em array de Int */
-    public static int[] convertStringToInt(String[] tempLinhas, int n){
-        int[] array = new int[n];
-        for(int i = 0; i < tempLinhas.length; i++){
-            for(int j = 0; j < tempLinhas[i].length(); j++){
-                array[i] = (int)tempLinhas[i].charAt(j);
-            }
-        }
-        return array;
     }
 
-    /* Funcao que retorna o espaco total  do array de String
-     * somado com o espaco total ocupado por cada caracter
-    */
-    public static int espacoTotalString(String[] temp){
-        int tempCount = 0;
-        for(int i = 0; i < temp.length; i++){
-            for(int j = 0; j < temp[i].length(); j++){
-                tempCount++;
+    /* Metodo para criptografar a String recebida
+     */
+    public static String encriptografaPrimeiraVez(String temp){
+        char[] charArray = temp.toCharArray();
+        for(int i = 0; i < charArray.length; i++){
+            if((charArray[i] >= 65 && charArray[i] <= 90) || (charArray[i] >= 97 && charArray[i] <= 122)){
+                charArray[i] += 3;
             }
         }
-        return tempCount;
+        return new String(charArray);
+    }
+
+    /* Metodo que recebe o indice do array de string e retorna
+     * a string do indice escrita ao contrario
+     */
+    public static String reverteString (String inverte){
+        char[] charArray = inverte.toCharArray();
+        int lenArray = charArray.length-1;
+        for(int i = 0; i <= lenArray; i++){
+            charArray[i] = inverte.charAt(lenArray - i);
+        }
+
+        return new String(charArray);
+    }
+
+    /* Metodo que recebe o indice do novo array de String
+     * e retorna a ultima criptografia
+     */
+    public static String encriptografaSegundaVez(String temp){
+        char[] arrayChar = temp.toCharArray();
+        for(int i = arrayChar.length/2 ; i < arrayChar.length; i++){
+            arrayChar[i] -= 1;
+        }
+        return new String(arrayChar);
+    }
+
+    /* Metodo para imprimir a String criptografada
+     */
+    public static void imprimir(String[] imprime){
+        for(int i = 0; i < imprime.length; i++){
+            System.out.println(imprime[i]);
+        }
     }
 }
