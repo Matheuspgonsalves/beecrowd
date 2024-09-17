075f421791ae3ad1e94b77734a5b84642135662f
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-09-17 16:23:16 -0300

    Atualizacoes no exercicio 1252

--- Estruturas_e_bibliotecas/1252_sort_sort_e_sort/Main.java
+++ Estruturas_e_bibliotecas/1252_sort_sort_e_sort/Main.java
@@ -21,8 +21,45 @@ public class Main {
             }
 
             int[] vetorOrdenado = ordenaVetor(vetorNums);
+
+            for(int i = 0; i < vetorOrdenado.length; i++){
+                System.out.println(vetorOrdenado[i]);
+            }
             in = sc.nextLine();
         }
+    }
+
+    public static int[] ordenaVetor(int[] vetor){
+        int aux;
+        for(int i = 0; i < (vetor.length - 1); i++){
+            int menor = i;
+            for(int j = i + 1; i < vetor.length; i++){
+                int maior = j;
+                if(vetor[i] % 3 == 0 && vetor[j] % 3 == 0){
+                    if(vetor[j] > vetor[menor]){
+                        maior = j; 
+                        aux = vetor[menor];
+                        vetor[menor] = vetor[maior];
+                        vetor[maior] = aux;
+                    }
+                } else if (vetor[i] % 3 == 0) {
+                    maior = j; 
+                    aux = vetor[menor];
+                    vetor[menor] = vetor[maior];
+                    vetor[maior] = aux;
+                } else if(vetor[i] % 3 != 0 && vetor[j] % 3 != 0) {
+                    if(vetor[i] > vetor[j]){
+                        menor = j;
+                        aux = vetor[menor];
+                        vetor[menor] = vetor[j];
+                        vetor[j] = aux;
+                    }
+                }
+
+            }
+        }
+
+        return vetor;
     }
 
     public static boolean verificaFim(String in){
